f8ce468d2732b852939e6b920577313c492d4c52
lionelsaad354 <akunbisnis.sf@gmail.com>
2017-11-06 02:29:49 +0700

    upload BasketBallScoring

---
+++ app/src/main/java/net/lionelsaad/basketballscoring/SplashActivity.java
@@ -0,0 +1,25 @@
+package net.lionelsaad.basketballscoring;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },7000);//Delay SplasScreen dihitung menggunakan milisekon jika 7000 milisekon berarti akan delay selama 7 detik
+    }
+}
